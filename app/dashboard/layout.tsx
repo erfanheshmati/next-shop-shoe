eed574f5e042ea5109f00cab0ad0f205633fe8ee
Erfan Heshmati <heshmati74@gmail.com>
2024-08-15 17:02:44 +0330

    Admin dashboard index page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -70,6 +70,7 @@ export default async function DashboardLayout({
           </DropdownMenuContent>
         </DropdownMenu>
       </header>
+      <main className="my-5">{children}</main>
     </div>
   );
 }

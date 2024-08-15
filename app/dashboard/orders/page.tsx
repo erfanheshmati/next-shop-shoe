f9f014aa68c73ae15d32975fc6b38f38339e51c1
Erfan Heshmati <heshmati74@gmail.com>
2024-08-15 17:42:47 +0330

    Admin dashboard orders route

---
+++ app/dashboard/orders/page.tsx
@@ -0,0 +1,53 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function OrdersPage() {
+  return (
+    <Card>
+      <CardHeader className="px-7">
+        <CardTitle>Orders</CardTitle>
+        <CardDescription>Recent orders from your store</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Customer</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <p className="font-medium">Erfan Heshmati</p>
+                <p className="hidden md:flex text-sm text-muted-foreground">
+                  heshmati74@gmail.com
+                </p>
+              </TableCell>
+              <TableCell>Sale</TableCell>
+              <TableCell>Successful</TableCell>
+              <TableCell>2024-06-17</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+}
